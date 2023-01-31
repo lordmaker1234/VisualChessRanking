2691fe21a9ab5e056ec4aac3357d7f27d887f2e9
Akerre <yourdegenerate@gmail.com>
2023-01-31 19:15:15 +0000

    modified site test method

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("refreshButton").addEventListener("click", siteCheck);
 
 function siteCheck() {
-  if (document.URL.includes("lichess.org")) {
+  if (/lichess.org/.test(window.location.href)) {
     document.getElementById("sitename").innerHTML = Lichess.org;
-  } else if (document.URL.includes("chess.com")) {
+  } else if (/chess.com/.test(window.location.href)) {
     document.getElementById("sitename").innerHTML = Chess.com;
   } else {
     document.getElementById("currentlyPlaying").style.display = "none";
