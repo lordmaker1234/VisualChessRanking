1a72ac10dde6439f3413534ef16ccf757906ba7c
Akerre <yourdegenerate@gmail.com>
2023-01-31 19:20:31 +0000

    im so bad

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById("refreshButton").addEventListener("click", siteCheck);
 
 function siteCheck() {
   if (/lichess.org/.test(window.location.href)) {
-    document.getElementById("sitename").innerHTML = Lichess.org;
+    document.getElementById("sitename").innerHTML = "Lichess.org";
   } else if (/chess.com/.test(window.location.href)) {
-    document.getElementById("sitename").innerHTML = Chess.com;
+    document.getElementById("sitename").innerHTML = "Chess.com";
   } else {
     document.getElementById("currentlyPlaying").style.display = "none";
   }
