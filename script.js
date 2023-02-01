5c4529472eed17cbe498ebfb97b38523fb769bf9
Akerre <yourdegenerate@gmail.com>
2023-02-01 16:46:41 +0000

    Added tabs permission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.getElementById("refreshButton").addEventListener("click", siteCheck);
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  let url = tabs[0].url;
+  // use `url` here inside the callback because it's asynchronous!
+});
 
 function siteCheck() {
-  if (/lichess.org/.test(window.location.href)) {
+  if (/lichess.org/.test(url)) {
+    //Unable to get the script to properly detect the page
     document.getElementById("sitename").innerHTML = "Lichess.org";
-  } else if (/chess.com/.test(window.location.href)) {
+  } else if (/chess.com/.test(url)) {
     document.getElementById("sitename").innerHTML = "Chess.com";
   } else {
     document.getElementById("currentlyPlaying").style.display = "none";
